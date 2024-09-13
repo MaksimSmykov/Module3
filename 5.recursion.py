90885378312f404cac14c61f31b75de4e6927fbf
MaksimSmykov <smykovmaksim23@yandex.ru>
2024-09-13 18:22:30 +0300

    Update 5.recursion.py

--- 5.recursion.py
+++ 5.recursion.py
@@ -8,29 +8,3 @@ def get_multiplied_digits(number):
 
 result = get_multiplied_digits(40203)
 print(result)
-# def summa(n):
-#     if n == 0:
-#         return 0
-#     else:
-#         return n + summa(n - 1)
-#
-#
-# print(summa(5))
-# def recursion():
-#     recursion()
-#
-# recursion()
-# stack = []
-# stack.append(1)
-# print('Добавили элемент' , stack)
-# stack.append(2)
-# print('Добавили элемент' , stack)
-# stack.append(3)
-# print('Добавили элемент' , stack)
-# print(stack)
-# stack.pop()
-# print('Убрали элемент', stack)
-# stack.pop()
-# print('Убрали элемент', stack)
-# stack.pop()
-# print('Убрали элемент', stack)
